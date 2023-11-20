8807ca746179b5ad1aad79ad4da9feeaf845fd75
Pete Potipitak <67202848+Tepah@users.noreply.github.com>
2023-11-20 13:33:07 -0800

    Initial Commit

---
+++ Flashcardio/FlashcardioApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardioApp.swift
+//  Flashcardio
+//
+//  Created by Pete Potipitak on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
