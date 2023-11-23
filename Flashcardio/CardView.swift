a2c323b366468ead36186a4cb74e98e7c3f5f3ef
justindnguyen <justindnguyen03@gmail.com>
2023-11-22 19:47:06 -0800

    added flip and swipe animation

--- Flashcardio/CardView.swift
+++ Flashcardio/CardView.swift
@@ -11,22 +11,31 @@ struct CardView: View {
     
     let card: Card
 
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
     @State private var isShowingAnswer = false
+    @State private var offset = CGSize.zero
     
     var body: some View {
         ZStack {
-            RoundedRectangle(cornerRadius: 12)
+            RoundedRectangle(cornerRadius: 12, style: .continuous)
                 .fill(Color(hex: 0x565656))
                 .frame(width: 340, height: 160)
-                .offset(x: 0, y: 0)
+                .background(
+                    differentiateWithoutColor ? nil :
+                        RoundedRectangle(cornerRadius: 12, style: .continuous)
+                        .fill(offset.width > 0 ? Color.green : Color.red)
+                )
                 .shadow(radius: 20)
             VStack {
-                Text(card.question)
-                    .font(.largeTitle)
-                    .foregroundColor(.white)
                 if isShowingAnswer {
                     Text(card.answer)
-                        .font(.title)
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                        .rotationEffect(.degrees(180))
+                        .scaleEffect(x: -1, y: 1)
+                } else {
+                    Text(card.question)
+                        .font(.largeTitle)
                         .foregroundColor(.white)
                 }
             }
@@ -35,10 +44,32 @@ struct CardView: View {
         }
         .accessibility(addTraits: .isButton)
         .onTapGesture {
-            self.isShowingAnswer.toggle()
+            withAnimation(.easeInOut) {
+                self.isShowingAnswer.toggle()
+            }
         }
-        .animation(.spring()) // can't figure out how to use withAnimation or animation(_:value:)
-        // need to work on .gesture() for removing a card
+        .rotation3DEffect(
+            .degrees(isShowingAnswer ? 180 : 0), axis: (x: 1, y: 0, z: 0)
+        )
+        .frame(width: 340, height: 160)
+        .rotationEffect(.degrees(Double(offset.width / 5)))
+        .offset(x: offset.width, y: 0)
+        .opacity(2 - Double(abs(offset.width / 100)))
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    self.offset = gesture.translation
+                }
+                .onEnded { _ in
+                    if abs(self.offset.width) > 100 {
+                        // need to add logic to remove a card from deck or keep in deck
+                        // self.removal?()
+                    } else {
+                        self.offset = .zero
+                    }
+                    
+                }
+        )
     }
     
 }
