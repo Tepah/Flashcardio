2724f6f8d3206baf2e251358b8cdaeb4cf908b44
justindnguyen <justindnguyen03@gmail.com>
2023-11-26 22:45:22 -0800

    added card deck view and edit screen

---
+++ Flashcardio/CardsView.swift
@@ -0,0 +1,18 @@
+//
+//  CardsView.swift
+//  Flashcardio
+//
+//  Created by Justin Nguyen on 11/26/23.
+//
+
+import SwiftUI
+
+struct CardsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardsView()
+}
---
+++ Flashcardio/EditCardView.swift
@@ -0,0 +1,18 @@
+//
+//  EditCardView.swift
+//  Flashcardio
+//
+//  Created by Justin Nguyen on 11/26/23.
+//
+
+import SwiftUI
+
+struct EditCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditCardView()
+}
